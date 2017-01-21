4aec55bf4e972fe0cabcc355540fc4046b846a94
Dustin Heffron <heffjoker@gmail.com>
2017-01-21 09:31:15 -0500

    Added Enemy and enabled collision detection/destruction.  Added collision detection on torpedos.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int Health = 10;
+
+    private int _currHealth = 0;
+
+	// Use this for initialization
+	void Awake () {
+        tag = "enemy";
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("player_torpedo"))
+            TakeDamage();
+    }
+
+    private void OnEnable()
+    {
+        _currHealth = Health;
+    }
+	
+    private void TakeDamage()
+    {
+        --_currHealth;
+
+        if (_currHealth <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Torpedo.cs
+++ Assets/Scripts/Torpedo.cs
@@ -6,27 +6,63 @@ public class Torpedo : MonoBehaviour {
 
     public float Speed = 100f;
     public float TimeToLive = 2f;
+    public ParticleSystem ExplosionParticles;
 
     private Rigidbody2D _rigidBody = null;
+    private SpriteRenderer _sprite = null;
 
     public void Fire(Vector3 direction, Vector3 position)
     {
         _rigidBody.AddForce(direction * Speed);
-        StartCoroutine(DeactivateAfterTime());
+        StartCoroutine(DeactivateAfterTime(TimeToLive));
         transform.position = position;
 
         float angle = MathUtil.Vector2ToAngle(direction);
         transform.rotation = Quaternion.AngleAxis(angle-90, -Vector3.back);
     }
 
-    private IEnumerator DeactivateAfterTime()
-    {
-        yield return new WaitForSeconds(TimeToLive);
-        gameObject.SetActive(false);
-    }
-
 	// Use this for initialization
 	void Awake () {
         _rigidBody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
 	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("enemy"))
+        {
+            _rigidBody.velocity = Vector2.zero;
+
+            if (ExplosionParticles != null)
+                ExplosionParticles.Play();
+
+            StopAllCoroutines();
+            
+            StartCoroutine(DisableAfterParticles());
+        }
+    }
+
+    private void OnEnable()
+    {
+        _sprite.enabled = true;
+    }
+
+    #region Coroutines
+
+    private IEnumerator DeactivateAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator DisableAfterParticles()
+    {
+        _sprite.enabled = false;
+        while (ExplosionParticles.isPlaying)
+            yield return null;
+
+        gameObject.SetActive(false);
+    }
+
+    #endregion Coroutines
 }
