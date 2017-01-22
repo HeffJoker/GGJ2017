bbf9262c521f6a633fba895151fd22008eb5a6f2
Dustin Heffron <heffjoker@gmail.com>
2017-01-21 19:54:30 -0500

    Added game over.  Fixed game state management.

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateManager : SingletonBase<GameStateManager> {
 
     public Canvas PauseScreen;
-
+    public Canvas GameOverScreen;
+    public Canvas PlayScreen;
+    public Canvas WinScreen;
 
     public void Pause()
     {
@@ -22,5 +25,33 @@ public class GameStateManager : SingletonBase<GameStateManager> {
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        PlayScreen.enabled = false;
+        GameOverScreen.enabled = true;
+    }
+    
+    public void Win()
+    {
+        PlayScreen.enabled = false;
+        WinScreen.enabled = true;
+    }
+
+    public void LoadScene_Single(string sceneToChange)
+    {
+        // Load Scene
+        SceneManager.LoadScene(sceneToChange, LoadSceneMode.Single);
+    }
+
+    public void LoadScene_Additive(string sceneToChange)
+    {
+        SceneManager.LoadScene(sceneToChange, LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene(string sceneToUnload)
+    {
+        SceneManager.UnloadScene(sceneToUnload);
     }
 }
